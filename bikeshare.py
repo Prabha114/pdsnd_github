a5f03325e5364802279eea9f12a83f568fd8d984
prabhakaran <chiddu.prabhu@gmail.com>
2022-12-01 17:23:58 +0530

    refactor: Change to bikeshare.py code third time

--- bikeshare.py
+++ bikeshare.py
@@ -124,17 +124,16 @@ def station_stats(df):
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
-    # TO DO: display most commonly used start station
-
-    Start_Station = df['Start Station'].value_counts().idxmax()
-    print('Most Commonly used start station:', Start_Station)
-
 
     # TO DO: display most commonly used end station
 
     End_Station = df['End Station'].value_counts().idxmax()
     print('\nMost Commonly used end station:', End_Station)
+    
+    # TO DO: display most commonly used start station
 
+    Start_Station = df['Start Station'].value_counts().idxmax()
+    print('Most Commonly used start station:', Start_Station)
 
     # TO DO: display most frequent combination of start station and end station trip
 
